1e3e2200fb47537782f2f0f0cc964b6294d3f954
ARodolfo-Nunes <rodolfomoutonuno13@gmail.com>
2025-06-28 19:14:15 -0300

    Atualizando css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 const btn = document.querySelector('.btn');
 const nav = document.querySelector('.nav');
 
+function fecharMenu () {
+  btn.classList.remove('active');
+  nav.classList.remove('show');
+}
+
 btn.addEventListener('click',function () {
   btn.classList.toggle('active');
   nav.classList.toggle('show');
 });
+
+document.addEventListener('click', function(e) {
+  if (!btn.contains(e.target) && !nav.contains(e.target)) {
+    fecharMenu();
+  }
+})
+
+nav.addEventListener('click', function () {
+  fecharMenu();
+})
