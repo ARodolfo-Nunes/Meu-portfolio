02a0bf99ef73d9bdd48a17b8e50dc23317512fcc
ARodolfo-Nunes <rodolfomoutonuno13@gmail.com>
2025-06-28 17:37:36 -0300

    Adicionando informações ao portfólio

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@ const btn = document.querySelector('.btn');
 const nav = document.querySelector('.nav');
 
 btn.addEventListener('click',function () {
+  btn.classList.toggle('active');
   nav.classList.toggle('show');
 });
